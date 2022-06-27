8fcb3521d7e685f3f490d5344cc0ba13654fea2b
Md Haider Ali <haider.aiub2014@gmail.com>
2022-06-27 02:35:09 -0400

    middle state

--- src/temp/GraphEmbed.py
+++ src/temp/GraphEmbed.py
@@ -1,15 +1,18 @@
 import pickle
 import networkx as nx
 import matplotlib.pyplot as plt
+from graph_tools import Graph
+from pecanpy.pecanpy import SparseOTF
+from tqdm import tqdm
+
 from src.preprocess.DataPreprocess import DataPreprocess
 import itertools
-
+from karateclub import DeepWalk, Node2Vec
+from scipy import spatial
 
 class GraphEmbed:
 
     def embed(self):
-
-        # self.analyze_graph()
 
         obj_preprocess = DataPreprocess()
         api_call_sequence = obj_preprocess.preprocess_api_call_sequence()
@@ -18,18 +21,33 @@ class GraphEmbed:
         print('encoding start')
         # Label encode
         invoked_methods_sentences = self.encode_unique_label(invoked_methods_sentences)
+
         print('encoding finished')
 
         G = self.build_graph(invoked_methods_sentences)
 
-        # ss = G.edges(data=True)
+        ss = G.edges(data=True)
         # print(ss)
 
+
         nx.write_gpickle(G, 'D:\Study\Final Project\Dataset\Ground Truth\GoogleLangCommonLang\graph.gpickle')
-
-        self.plot_graph(G)
-
         print('graph dumpped')
+
+        edge_list_file = "D:\Study\Final Project\Dataset\Ground Truth\GoogleLangCommonLang\graphedzz.edgelist"
+
+        nx.write_edgelist(G, edge_list_file)
+
+        g = SparseOTF(p=1, q=1, workers=1, verbose=False)
+        g.read_edg(edge_list_file, weighted=False, directed=False, delimiter=' ')
+
+        # generate random walks
+        walks = g.simulate_walks(num_walks=10, walk_length=80)
+        # use random walks to train embeddings
+        a= 100
+
+        # self.plot_graph(G)
+
+        # self.analyze_graph()
 
     def encode_unique_label(self, sentences):
         identifiers = {}
@@ -39,23 +57,42 @@ class GraphEmbed:
                 if word not in identifiers:
                     identifiers[word] = idx
                     idx += 1
+
+        print(identifiers)
+
         list_values = []
         for sent in sentences:
-            values = [str(identifiers[word]) for word in sent.split()]
-            str_values = " ".join(values)
-            list_values.append(str_values)
+            values = [identifiers[word] for word in sent.split()]
+            list_values.append(values)
         return list_values
 
     def analyze_graph(self):
         G = nx.read_gpickle('D:\Study\Final Project\Dataset\Ground Truth\GoogleLangCommonLang\graph.gpickle')
-        sim = nx.simrank_similarity(G, '0', '1')
-        print('graph simlarity')
+
+        # ss = G.edges(data=True)
+        # print(ss)
+
+        sim = nx.simrank_similarity(G, 1, 56)
+        print('graph similarity', sim)
         print(sim)
+
+        # model = DeepWalk()  # node embedding algorithm
+        # model = DeepWalk(window_size=4)  # node embedding algorithm
+        # model.fit(G)  # fit it on the graph
+        # embedding = model.get_embedding()  # extract embeddings
+        #
+        # print('Number of karate club members:', len(G.nodes))
+        # print('Embedding array shape:', embedding.shape)
+        #
+        # cosine_similarity = 1 - spatial.distance.cosine(embedding[4], embedding[i])
+        # print(i, "   ", cosine_similarity)
+
+        a = 9
 
     def get_node_relation_custom(self, document):
         mapped_pairs = []
         for sent in document:
-            split_str = sent.split(" ")
+            split_str = sent
             if len(split_str) == 0 or len(split_str) == 1:
                 continue
             if len(split_str) == 2:
@@ -75,13 +112,13 @@ class GraphEmbed:
         # get graph nodes
         # nodes = self.get_entities(document)
         nodes = self.get_entities_custom(document)
-        print('nodes finished')
+        print('nodes finished', len(nodes))
 
         print('edges  started')
         # get graph edges
         # edges = self.get_relations(document)
         edges = self.get_node_relation_custom(document)
-        print('edges  finished')
+        print('edges  finished', len(edges))
 
         # create graph structure with NetworkX
         G = nx.Graph()
@@ -91,7 +128,7 @@ class GraphEmbed:
         return G
 
     def get_entities_custom(self, document):
-        unique_words = set(itertools.chain.from_iterable(map(str.split, document)))
+        unique_words = {x for l in document for x in l}
         return unique_words
 
     def get_entities(self, document):
@@ -106,7 +143,7 @@ class GraphEmbed:
     def get_relations(self, document):
         # in our case, relations are bigrams in sentences
         bigrams = []
-        for sent in document:
+        for sent in tqdm(document):
             sent = sent.split(" ")
             for i in range(len(sent) - 1):
                 # for every word and the next in the sentence
